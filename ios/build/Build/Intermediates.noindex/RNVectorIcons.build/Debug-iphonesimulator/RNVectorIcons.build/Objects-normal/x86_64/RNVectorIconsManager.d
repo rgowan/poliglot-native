25f605fdaf51c2bb655fcc3c1f30630974df4399
rgowan <ranegowan@gmail.com>
2018-06-20 12:23:50 +0100

    added vector icons

---
+++ ios/build/Build/Intermediates.noindex/RNVectorIcons.build/Debug-iphonesimulator/RNVectorIcons.build/Objects-normal/x86_64/RNVectorIconsManager.d
@@ -0,0 +1,21 @@
+dependencies: \
+  /Users/ranegowan/Development/github-repos/poliglot-native/node_modules/react-native-vector-icons/RNVectorIconsManager/RNVectorIconsManager.m \
+  /Users/ranegowan/Development/github-repos/poliglot-native/node_modules/react-native-vector-icons/RNVectorIconsManager/RNVectorIconsManager.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTLog.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTAssert.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTUtils.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTConvert.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTAnimationType.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBorderStyle.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTPointerEvents.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTTextDecorationLineType.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/Yoga.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGEnums.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGMacros.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridge.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeDelegate.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTJavaScriptLoader.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTFrameUpdate.h \
+  /Users/ranegowan/Development/github-repos/poliglot-native/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h
